5b451faa1732aacb9a321251a31e8a7e24d0bbde
artsolis8 <artsolis8@gmail.com>
2019-12-05 17:28:18 -0300

    Revert "Actualizacion"
    This reverts commit 95b3888e9fcddb931477099d952f5f38f67643f1.

--- WpfCionsultaMedica/Medico.cs
+++ WpfCionsultaMedica/Medico.cs
@@ -33,11 +33,5 @@ namespace WpfCionsultaMedica
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Cita> Cita { get; set; }
-
-        public override string ToString()
-        {
-            
-            return this.nombre;
-        }
     }
 }
--- WpfCionsultaMedica/Reserva.cs
+++ WpfCionsultaMedica/Reserva.cs
@@ -31,10 +31,5 @@ namespace WpfCionsultaMedica
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Cita> Cita { get; set; }
-        public override string ToString()
-        {
-            //es lo mas representativo de la clase persona.. podemos concatenar tambien varios campos..
-            return this.fecha_reservada.ToString();
-        }
     }
 }
